48235382692ecc0283f45aaca6e17355bd0ec2fc
David Rollinson <g7gpr@outlook.com>
2021-11-25 19:40:00 +0800

    Modified install procedure

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt-get install python3-pymap3d -y
 sudo apt-get install astrometry.net -y
 sudo apt-get install libvips-dev -y
+wget http://data.astrometry.net/4100/ /usr/share/astrometry
 mv fits2azel.sh /usr/local/bin
 mv fits2azel.py /usr/local/bin
